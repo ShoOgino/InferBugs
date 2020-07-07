d09f15f0bc9f82e6dc6edf4a4e1ed8bd37bcd56f
ShoOgino <loginologist@gmail.com>
2020-07-07 15:51:26 +0900

    update

--- actions/src/dataset.py
+++ actions/src/dataset.py
@@ -81,6 +81,7 @@ class Data:
         #print("selected commits")
         print("commits: "+str(len(self.commits)))
         self.commitsBug=commitsBug
+        print("-----------------------------------------------")
     def getData(self):
         return [
             self.calculateLOC(),
